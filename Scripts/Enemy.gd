16d2bf3140517218eb0b01d5efeb9bb3ef7d72f2
fingerson <henfinzim@gmail.com>
2019-01-25 23:40:50 -0200

    fiz umas paradas 3

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -25,7 +25,7 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	move_and_slide(vel_vec)
+	move_and_slide(-vel_vec)
 	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
